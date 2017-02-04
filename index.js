378219f6541e09fab5aece607a8afc733cecd942
Antti Mattila <antti.mattila@futurice.com>
2017-02-04 12:15:35 +0200

    Add express

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
+const express = require('express');
+const packageInfo = require('./package.json');
 const TelegramBot = require('node-telegram-bot-api');
-const telegram = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 const Brain = require('./brain');
+
+const telegram = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
 const getComic = (message) => {
   Brain.fetchComic().then(url => {
@@ -12,3 +15,13 @@ const getComic = (message) => {
 
 telegram.onText(/^\/pori/, getComic);
 telegram.onText(/^\/fingerpori/, getComic);
+
+const app = express();
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+const server = app.listen(process.env.PORT, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
